ab7d036bbbf8dc8c9cafd758262f18d098b26256
Jiayi Li <17785923961@163.com>
2022-01-02 19:00:42 -0800

    props added

--- FixedGraph.js
+++ FixedGraph.js
@@ -6,15 +6,29 @@ import colorsDic from './colors'
 class FixedGraph extends React.Component{
     constructor(props){
         super(props);
-        this.data = this.props.data;
-        this.entities = this.props.entities;
+        this.pack = this.props.pack;
+        if(this.props.data){
+            if(!this.props.entities) this.pack = Object.assign({}, this.props.data);
+            else{
+                this.pack = {};
+                for (let i=0; i<this.props.entities.length; i++) {
+                    this.pack[this.props.entities[i]] = this.props.data[i];
+                }
+            }
+        }
+        this.length = this.pack ? Object.values(this.pack)[0].length : this.data[0].length;
         this.titles = this.props.titles;
-        this.timer = this.props.timer ? this.props.timer : 1000;
+        this.timeGap = this.props.timeGap ? this.props.timeGap : 2000;
+        this.transition = this.props.transition ? this.props.transition: 500;
         this.delay = this.props.delay ? this.props.delay : 0;
         this.repeat = this.props.repeat ? this.props.repeat : true;
-        this.length = Object.values(this.data)[0].length;
-        this.colorTheme = this.props.theme && this.props.theme in colorsDic ? this.props.theme :'Red';
-        this.colors = Array.from({length: this.length}, () =>  colorsDic[this.colorTheme][Math.floor(Math.random()*8)])
+        if(this.props.colors){
+            this.colors = [];
+            for(let i=0;i<this.length;i++) this.colors[i] = this.props.colors[i%this.props.colors.length];
+        }else{
+            let colorTheme = this.props.theme && this.props.theme in colorsDic ? this.props.theme :'Red';
+            this.colors = Array.from({length: this.length}, () =>  colorsDic[colorTheme][Math.floor(Math.random()*8)])
+        }
         this.state={
             round: 0,
             maxAmount: this.getMax(0),
@@ -24,14 +38,14 @@ class FixedGraph extends React.Component{
     }
     componentDidMount(){
         if(this.state.started){
-          const intervalId = setInterval(this.nextOne, this.timer + this.delay);
+          const intervalId = setInterval(this.nextOne, this.timeGap + this.delay);
           this.setState({intervalId: intervalId});
         }
       }
   
     componentWillReceiveProps(nextProps) {
         if (nextProps.started) {
-            const intervalId = setInterval(this.nextOne, this.timer + this.delay);
+            const intervalId = setInterval(this.nextOne, this.timeGap + this.delay);
             this.setState({intervalId: intervalId});
         }
     }
@@ -42,7 +56,7 @@ class FixedGraph extends React.Component{
 
     nextOne(){
         console.log(this.state.round)
-        if(!this.repeat && this.state.round === this.data.length-1 ){
+        if(!this.repeat && this.state.round === this.pack.length-1 ){
             clearInterval(this.state.intervalId);
             return;
         }
@@ -56,28 +70,33 @@ class FixedGraph extends React.Component{
 
     getMax(round){
         let currMax = Number.MIN_VALUE;
-        Object.values(this.data).forEach(ele => currMax = Math.max(currMax,ele[round]));
+        Object.values(this.pack).forEach(ele => currMax = Math.max(currMax,ele[round]));
         return currMax;
     }
     // getAttributes(entity){ }
     render(){
         console.log('state: ',this.state); 
         
-        let List = Object.keys(this.props.data).map((entity,index) => {
+        let List = Object.keys(this.pack).map((entity,index) => {
             return(
                 <Item
                 key={index}
                 title={entity}
-                data={this.data[entity][this.state.round]}
+                pack={this.pack[entity][this.state.round]}
                 maxAmount={this.state.maxAmount}
                 color={this.colors[index]}
+                transition={this.transition}
+                delay={this.delay}
+                titleStyle={this.props.titleStyle}
+                descriptionStyle={this.props.descriptionStyle}
+                barStyle={this.props.barStyle}
                 />
             )
         });
-        if(!this.data) return null;
+        if(!this.pack) return null;
         return(
             <div>
-                {this.titles && <div className='titleText'>{this.titles[this.state.round]}</div>}
+                {this.titles && <div className='titleText' style={{...this.props.headStyle}}>{this.titles[this.state.round]}</div>}
                 <div className='container'>
                     {List}
                 </div>
--- Item.js
+++ Item.js
@@ -4,17 +4,19 @@ import './style.css'
 
 function Item(props){
     // console.log('props: ',props);
-    
+    let {icon,title,color,pack,delay,transition,maxAmount,titleStyle,descriptionStyle,barStyle} = props;
+    if(!color) color = Math.floor(Math.random()*16777215).toString(16);
+    const colorDivStyle = {transitionProperty:'width',width:`${pack*100.0/maxAmount}%`,backgroundColor:color,transitionDuration:transition+'ms',transitionDelay:delay+'ms' };
     return(
         <div className='itemContainer'>
-            {props.icon && <div className='icon'></div>}
-            {props.title && <div className='title'>{props.title}</div>}
-            <div className='bar'>
-                <div className='colorDiv' style={{width:`${props.data*100.0/props.maxAmount}%`,backgroundColor:props.color }}>
+            {icon && <div className='icon'></div>}
+            {title && <div className='title' style={{...titleStyle}}>{title}</div>}
+            <div className='bar' style={barStyle}>
+                <div className='colorDiv' style={colorDivStyle}>
                 &nbsp;
                 </div>
             </div>
-            <div className='data'>{props.data}</div>
+            <div className='pack' style={descriptionStyle}>{pack}</div>
         </div>
     );
 }
